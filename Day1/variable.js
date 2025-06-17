7a3adc20af6184b3a0e8f8ae44e9ee2edba26ddb
DivyeSharma <shashank.sharma5555@gmail.com>
2025-06-17 10:12:07 +0530

    Variable

---
+++ Day1/variable.js
@@ -0,0 +1,13 @@
+//Declare Variable with - let, var, const
+
+let college = "lpu";
+college = "IIT";
+console.log(college);
+
+
+var name = "Aditya Verma";
+console.log(name);
+
+
+const RollNo = "05";
+console.log(RollNo);
